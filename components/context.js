8b635f61f89ef35cc26544c82766457c9e31f8d9
Vinícius Hoyer <vhoyer@live.com>
2018-04-06 00:26:02 +0000

    Removed debuging console.log

--- components/context.js
+++ components/context.js
@@ -24,14 +24,12 @@ Vue.component('context-menu',{
         x:function(){
             let outsideScreen = window.innerWidth - 18 - (this.x + this.width)
             if (outsideScreen < 0){
-                console.log(outsideScreen)
                 this.$emit('outside-x', outsideScreen)
             }
         },
         y:function(){
             let outsideScreen = window.innerHeight - 18 - (this.y + this.height)
             if (outsideScreen < 0){
-                console.log(outsideScreen)
                 this.$emit('outside-y', outsideScreen)
             }
         },
