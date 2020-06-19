6d69f9e37911cbd5e585e6d50792db607f2929f4
Vinícius Hoyer <contact@vhoyer.dev>
2020-06-18 22:12:04 -0300

    :sparkles: Add forshadoing for future releases

--- src/components/my-footer.vue
+++ src/components/my-footer.vue
@@ -6,7 +6,7 @@
           class="col"
           data-hj-whitelist
         >
-          TimedReport!™ — v{{ tagName }}
+          TimedReport!™ — next
         </small>
         <small class="col text-right">
           Vinícius Hoyer
@@ -15,59 +15,3 @@
     </div>
   </footer>
 </template>
-
-<script>
-function request(obj) {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open(obj.method || 'GET', obj.url);
-    if (obj.headers) {
-      Object.keys(obj.headers).forEach((key) => {
-        xhr.setRequestHeader(key, obj.headers[key]);
-      });
-    }
-    xhr.onload = () => {
-      if (xhr.status >= 200 && xhr.status < 300) {
-        resolve(xhr.response);
-      } else {
-        reject(xhr.statusText);
-      }
-    };
-    xhr.onerror = () => reject(xhr.statusText);
-    xhr.send(obj.body);
-  });
-}
-
-export default {
-  data() {
-    return {
-      tagName: '2018.5.1',
-    };
-  },
-  mounted() {
-    request({
-      url: 'https://api.github.com/graphql',
-      method: 'POST',
-      headers: {
-        Authorization: 'bearer 15d45d5940bd6195d88851801f81ac85f1c950b4',
-      },
-      body: JSON.stringify({
-        query: `query {
-                    repository(owner: "vhoyer", name: "timedReport") {
-                      refs(refPrefix: "refs/tags/", first: 1, orderBy: {field: TAG_COMMIT_DATE, direction: DESC}) {
-                        edges {
-                          node {
-                            name
-                          } } } } }`,
-      }),
-    })
-      .then((raw) => {
-        const { data } = JSON.parse(raw);
-        this.tagName = data.repository.refs.edges[0].node.name;
-      });
-  },
-};
-</script>
-
-<style lang="scss" scoped>
-</style>
