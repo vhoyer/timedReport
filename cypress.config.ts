143903f31051a5cea084cc2fac1c281039d28857
Vinícius Hoyer <contact@vhoyer.dev>
2022-10-13 22:01:24 -0300

    :wrench: disallow third-party requests to ga and doubliclick.net

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,10 @@ import { initPlugin } from '@frsource/cypress-plugin-visual-regression-diff/plug
 
 export default defineConfig({
   e2e: {
+    blockHosts: [
+      '*.google-analytics.com',
+      '*.doubleclick.net',
+    ],
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     baseUrl: `http://localhost:${process.env.PORT ?? '4173'}`,
     setupNodeEvents(on, config) {
