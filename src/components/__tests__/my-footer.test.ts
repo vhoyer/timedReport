0d8fbc260a7f464136ddb835d2992c0562ac8034
Vinícius Hoyer <contact@vhoyer.dev>
2023-04-20 08:05:03 -0300

    :arrow_up: upgrade lazy-let with "afterEach" resetting

--- src/components/__tests__/my-footer.test.ts
+++ src/components/__tests__/my-footer.test.ts
@@ -1,10 +1,10 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, afterEach } from 'vitest';
 import { lazylet } from '@vhoyer/lazy-let';
 import { render } from '@testing-library/vue';
 import MyFooter from '../my-footer.vue';
 
 describe('Components > Footer', () => {
-  const $ = lazylet({
+  const $ = lazylet(afterEach, {
     wrapper: () => render(MyFooter),
   });
 
